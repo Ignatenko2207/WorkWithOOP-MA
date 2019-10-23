63c9c1fd16a0db2d55a3bbb3a37e6b61553a5b08
Alex <ignatenko2207@gmail.com>
2019-10-23 19:22:12 +0300

    Add spaces

--- test/com/mainacad/circle/CircleTest.java
+++ test/com/mainacad/circle/CircleTest.java
@@ -6,8 +6,10 @@ class CircleTest {
 
     @org.junit.jupiter.api.Test
     void getArea() {
+
         Circle circle = new Circle(10);
         assertEquals(314.15926, circle.getArea(), 0.00001);
         assertTrue(circle.getArea() > 314.15925 && circle.getArea() < 314.15927);
+
     }
 }
